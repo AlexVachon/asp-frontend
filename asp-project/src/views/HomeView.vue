8633dba0bddb71f355543ad56b45cf875bdb91f5
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-11-13 20:31:57 -0500

    Init

---
+++ asp-project/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <div class="card shadow mx-auto" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">Colors</h5>
+        <p class="text-primary">.text-primary</p>
+        <p class="text-primary-emphasis">.text-primary-emphasis</p>
+
+        <p class="text-secondary">.text-secondary</p>
+        <p class="text-secondary-emphasis">.text-secondary-emphasis</p>
+
+        <p class="text-success">.text-success</p>
+        <p class="text-success-emphasis">.text-success-emphasis</p>
+
+        <p class="text-danger">.text-danger</p>
+        <p class="text-danger-emphasis">.text-danger-emphasis</p>
+
+        <p class="text-warning">.text-warning</p>
+        <p class="text-warning-emphasis">.text-warning-emphasis</p>
+
+        <p class="text-info">.text-info</p>
+        <p class="text-info-emphasis">.text-info-emphasis</p>
+
+        <p class="text-light bg-dark">.text-light</p>
+        <p class="text-light-emphasis bg-dark">.text-light-emphasis</p>
+
+        <p class="text-dark">.text-dark</p>
+        <p class="text-dark-emphasis">.text-dark-emphasis</p>
+
+        <p class="text-body">.text-body</p>
+        <p class="text-body-emphasis">.text-body-emphasis</p>
+
+        <p class="text-body-secondary">.text-body-secondary</p>
+        <p class="text-body-tertiary">.text-body-tertiary</p>
+
+        <p class="text-black">.text-black</p>
+        <p class="text-white bg-dark">.text-white</p>
+
+        <p class="text-black-50">.text-black-50</p>
+        <p class="text-white-50 bg-dark">.text-white-50</p>
+      </div>
+    </div>
+  </main>
+</template>
