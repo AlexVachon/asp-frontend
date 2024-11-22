2e58bd4ef898b57a8ecbf3f7c402f9350e98888c
Alexandre Vachon <alvac002@edu.cegepgarneau.ca>
2024-11-21 20:02:19 -0500

    Save theme to localStorage

--- asp-project/src/components/Tools/SwitchTheme.vue
+++ asp-project/src/components/Tools/SwitchTheme.vue
@@ -1,30 +1,36 @@
 <template>
-    <div >
-      <b-form-checkbox 
-        v-model="checked"
-        name="check-theme"  
-        switch 
-        size="m"
-        @change="toggleTheme"
-        >
-        <span>
-            <i :class="iconClass" class="ms-2"></i>
-        </span>
-      </b-form-checkbox>
-    </div>
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref, computed } from 'vue';
-  
-  const checked = ref(false);
+  <div>
+    <b-form-checkbox v-model="checked" name="check-theme" switch size="m" @change="toggleTheme">
+      <span>
+        <i :class="iconClass" class="ms-2"></i>
+      </span>
+    </b-form-checkbox>
+  </div>
+</template>
 
-  const iconClass = computed(() => (checked.value ? 'bi-moon-fill': 'bi-sun-fill'))
 
-  const toggleTheme = () => {
-    const theme = checked.value ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-bs-theme', theme)
-  };
+<script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue';
 
-  </script>
-  
+const checked = ref(false);
+
+onMounted(() => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    checked.value = true;
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+  } else {
+    checked.value = false;
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+  }
+});
+
+const iconClass = computed(() => (checked.value ? 'bi-moon-fill' : 'bi-sun-fill'));
+
+const toggleTheme = () => {
+  const theme = checked.value ? 'dark' : 'light';
+  document.documentElement.setAttribute('data-bs-theme', theme);
+  localStorage.setItem('theme', theme);
+};
+
+</script>
